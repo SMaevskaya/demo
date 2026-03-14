b489ce84b067f15b4a92f166452ba6f1cd988692
svetl <svetlanamaevsckaya@yandex.ru>
2026-03-14 19:29:58 +0300

    Swagger, HATEOAS

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/users")
@@ -26,34 +29,45 @@ public class UserController {
 
 
     @GetMapping
-    List<UserDto> getAllUsers(){
-        return userService.getAllUsers();
+    List<EntityModel<UserDto>> getAllUsers(){
+        List<EntityModel<UserDto>> collect = userService.getAllUsers()
+                .stream()
+                .map(UserController::toHateoasEntityModel)
+                .collect(Collectors.toList());
+        return collect;
     }
 
 
 
     @GetMapping("/{id}")
-    UserDto getUserById(@PathVariable int id){
-        return userService.getUserById(id);
+    EntityModel<UserDto> getUserById(@PathVariable int id){
+        return toHateoasEntityModel(userService.getUserById(id));
     }
 
     @PostMapping()
-    UserDto saveUser(@RequestBody UserDto userDto) {
+    EntityModel<UserDto> saveUser(@RequestBody UserDto userDto) {
         UserDto  userDto1=userService.saveUser(userDto);
         kafkaMessagingService.sendMessage("save-user","save "+userDto1.getEmail());
-        return userDto1;
+        return toHateoasEntityModel(userDto1);
     }
 
     @DeleteMapping("/{id}")
-    void deleteUserById(@PathVariable int id) {
+    ResponseEntity<?> deleteUserById(@PathVariable int id) {
         UserDto userDto =userService.getUserById(id);
         userService.deleteUserById(id);
         kafkaMessagingService.sendMessage("delete-user","delete "+userDto.getEmail());
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
-    UserDto updateUser(@PathVariable int id,@RequestBody UserDto userDto) {
-       return userService.updateUser(userDto);
+    EntityModel<UserDto> updateUser(@PathVariable int id,@RequestBody UserDto userDto) {
+       return toHateoasEntityModel(userService.updateUser(userDto));
+    }
+
+    private static EntityModel<UserDto> toHateoasEntityModel(UserDto userDto) {
+        Link selfLink = linkTo(methodOn(UserController.class).getUserById(userDto.getId())).withSelfRel();
+        Link allUsersLink = linkTo(methodOn(UserController.class).getAllUsers()).withRel("all-users");
+        return EntityModel.of(userDto, selfLink, allUsersLink);
     }
 
 }
